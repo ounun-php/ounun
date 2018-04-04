b60c2826c2eae74d15f2b99f876ab261dca97af1
dreamxyp <dreamxyp@gmail.com>
2018-04-04 11:40:04 +0800

    Captcha
    Captcha

--- libs.v2/plugins/image/captcha.class.php
+++ libs.v2/plugins/image/captcha.class.php
@@ -20,57 +20,23 @@ class captcha
 	 */
 	public static function output($cookie = 'captcha', $img_width = 75, $img_height = 24, $img_lenght = 4)
 	{
-		if(!function_exists("imagecreatetruecolor"))
-		{
-			$input = array('1335', '3114', '4922', '2320', '1268', '9011');
-			$input = $input[array_rand($input)];
-			setcookie($cookie,md5($input));
-			//setcookie($cookie,md5($input),time()+3600);
-			header('Content-type: image/png');
-			readfile(Dir_Plugins_Captcha . "res/no_gd/{$input}.png");
-			exit();
-		}
-		$font = array(
-						Dir_Plugins_Captcha . 'res/font/a.ttf',
-						Dir_Plugins_Captcha . 'res/font/b.ttf',
-						Dir_Plugins_Captcha . 'res/font/c.ttf'
-					 );
-		$input = null;
-		for($ti = 0; $ti < $img_lenght; $ti++)
-		{
-			$input .= dechex(mt_rand(0, 9));
-		}			
-		$number_img = imagecreatetruecolor($img_width + 10, $img_height);
-		$white 		= imagecolorallocate($number_img, 255, 255, 255);
-		imagefilledrectangle($number_img, 0, 0, $img_width + 10 - 1, $img_height - 1, $white);
-		for($i = 1; $i <= 100; $i++)
-		{
-			\imagefttext($number_img, mt_rand(5, 10), mt_rand(0, 60 ), mt_rand(1, $img_width), mt_rand(1, $img_height), imagecolorallocate($number_img, mt_rand(180, 255), mt_rand(180, 255), mt_rand(180, 255)), $font[mt_rand(0, 2)], "*");
-		}
-		for($i = 1; $i <= 100; $i++)
-		{
-			\imagefttext($number_img, mt_rand(5, 10), mt_rand(0, 180), mt_rand(1, $img_width), mt_rand(1, $img_height), imagecolorallocate($number_img, mt_rand(180, 255), mt_rand(180, 255), mt_rand(180, 255)), $font[mt_rand(0, 2)], "\\");
-		}
-		for($i = 0; $i < strlen($input); $i++)
-		{
-			\imagefttext($number_img, mt_rand(14, 16), mt_rand(-30, 30), $i * $img_width / 4 + mt_rand(5, 15), mt_rand($img_height * 4 / 5, $img_height * 8 / 9), imageColorAllocate($number_img, mt_rand(0, 100), mt_rand(0, 150), mt_rand(0, 200)), $font[mt_rand(0, 2)], $input[$i]);
-		}
-		setcookie($cookie,md5($input));
-		// setcookie($cookie,md5($input),time()+3600);
-		header("Content-type: image/png");
-		imagepng($number_img);
-		imagedestroy($number_img);
+        $base = new base();
+        $base->make($img_width, $img_height, $img_lenght);
+        setcookie($cookie,md5($base->code),time()+3600,'/');
+        // setcookie($cookie,md5($base->code),time()+3600);
+        $base->output();
 	}
+
 	/**
 	 * 确认认证码
 	 *
-	 * @param string $Captcha
+	 * @param string $code
 	 * @param string $cookie
 	 * @return boolean
 	 */
-	public static function check($Captcha,$cookie = 'captcha')
+	public static function check($code, $cookie = 'captcha')
 	{
-		$rs = ($Captcha && $_COOKIE[$cookie] && md5($Captcha) == $_COOKIE[$cookie])?true:false;
+		$rs = ($code && $_COOKIE[$cookie] && md5($code) == $_COOKIE[$cookie])?true:false;
 		setcookie($cookie,'',-3600);
 		return $rs;
 	}
